8817d1282165e659b27f6dceb2f2c956e93be632
burakhanunver <burakhanunver@gmail.com>
2023-06-22 17:08:37 +0300

    mobile home page end

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 import logo from "@/assets/email.png"
 import news from "@/assets/news.jpg"
@@ -6,14 +7,15 @@ import news from "@/assets/news.jpg"
 import FeatureSection from "@/components/FeatureSection"
 import Testimonial from "@/components/Testimonial"
 import BottomSection from "@/components/BottomSection"
+
 import FAQ from "@/components/FAQ"
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-0 mx-0">
+    <main className="container mx-auto px-0 mb-6">
       <Image src={logo} alt="logo" className="w-16 mx-auto mt-6" />
       <h1 className="font-bold text-4xl text-center mt-6">Get Breaking News Easily!</h1>
-      <p className="text-center font-normal text-lg w-full mt-6">
+      <p className="text-center font-normal text-lg w-full mt-6 px-4">
         Your Personalized News Update Delivered to Your Inbox! Just Add Your Email and Start Getting Mails!
       </p>
       <section className="flex flex-col mt-6">
@@ -37,6 +39,7 @@ export default function Home() {
       <Testimonial />
       <BottomSection />
       <FAQ />
+      <Link href="/about" className="font-medium text-lg underline text-center block" >About this website</Link>
     </main>
   )
 }
--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import questionLogo from "@/assets/question.png"
+import Image from "next/image"
 
 function FAQ() {
   return (
-    <div>FAQ</div>
+    <section className="my-6 px-4 flex-col flex space-y-2 text-center">
+      <h2 className="font-bold text-4xl underline mb-4">FAQs</h2>
+
+      <Image src={questionLogo} alt="question mark logo" className="w-8 mx-auto" />
+      <h3 className="font-semibold text-lg mt-4">Can I customize the news categories I receive?</h3>
+      <p className="">
+        Yes, you can choose your preferred news categories and receive updates specifically tailored to your interests.
+      </p>
+
+      <hr className="border-1 border-gray-400 rounded-full mx-4" />
+
+      <Image src={questionLogo} alt="question mark logo" className="w-8 mx-auto" />
+      <h3 className="font-semibold text-lg  mt-4">How often will I receive news updates?</h3>
+      <p>You have the flexibility to select your preferred frequency: daily or weekly.</p>
+
+      <hr className="border-1 border-gray-400 rounded-full mx-4" />
+
+      <Image src={questionLogo} alt="question mark logo" className="w-8 mx-auto" />
+      <h3 className="font-semibold text-lg  mt-4">Can I cancel my subscription?</h3>
+      <p>
+        Yes, you can cancel your subscription at any time. Simply follow the cancellation instructions provided in the
+        app or on the website.
+      </p>
+
+      <hr className="border-1 border-gray-400 rounded-full mx-4" />
+
+      <Image src={questionLogo} alt="question mark logo" className="w-8 mx-auto" />
+      <h3 className="font-semibold text-lg  mt-4">Are the news sources reliable?</h3>
+      <p>
+        We prioritize accuracy and quality. Email News sources articles from reputable and trusted news sources,
+        ensuring the reliability of the information you receive.
+      </p>
+
+      <hr className="border-1 border-gray-400 rounded-full mx-4" />
+
+      <Image src={questionLogo} alt="question mark logo" className="w-8 mx-auto" />
+      <h3 className="font-semibold text-lg  mt-4">Is my personal information secure?</h3>
+      <p className="">
+        We take your privacy seriously. Rest assured that your personal information is protected and handled in
+        accordance with our strict privacy policy.
+      </p>
+    </section>
   )
 }
 
